33984c18ea4bcd960ec6ce898bc4e2eaca9b65a2
ShadowConYT <iamajay6122@gmail.com>
2024-02-21 19:50:51 +0530

    Add .env and testing files to .gitignore, and update requirements.txt and app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+from transformers import pipeline
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.llms import HuggingFaceHub
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -8,10 +9,13 @@ from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.chains import ConversationalRetrievalChain
 load_dotenv()
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import pyttsx3 as tts
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 import os
-
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 def load_doc(pdf_doc):
@@ -22,16 +26,27 @@ def load_doc(pdf_doc):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     text = text_splitter.split_documents(documents)
     db = Chroma.from_documents(text, embedding)
-    llm = HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-1-pythia-12b", model_kwargs={"temperature": 1.0, "max_length": 256})
+    #llm = HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-1-pythia-12b", model_kwargs={"temperature": 1.0, "max_length": 1250})
+    # load my local model from directory
+    tokenizer = AutoTokenizer.from_pretrained("bloom")
+    model = AutoModelForCausalLM.from_pretrained("bloom")
+
     global chain
     #chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
-    chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff",retriever=db.as_retriever())
+    chain = RetrievalQA.from_chain_type(llm=model,chain_type="stuff",retriever=db.as_retriever())
     return 'Document has successfully been loaded'
+
+def text_to_speech(text):
+    engine = tts.init()
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
 
 def answer_query(query):
     question = query
     res = chain.run(question)
-    res.split("Question:")[1].split("Helpful Answer:")[1].strip()
+    res = res.split("Question:")[1].split("Helpful Answer:")[1].strip()
+    text_to_speech(res)
     return res
 html = """
 <div style="text-align:center; max width: 700px;">
@@ -53,6 +68,7 @@ with gr.Blocks(css=css,theme=gr.themes.Monochrome()) as demo:
         with gr.Row():
             input = gr.Textbox(label="type in your question")
             output = gr.Textbox(label="output")
+
         submit_query = gr.Button("submit")
 
         load_pdf.click(load_doc,inputs=pdf_doc,outputs=status)
